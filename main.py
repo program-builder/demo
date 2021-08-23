c74359637bafc3e223a917b05262159ae3abd0f7
program-builder <89391098+program-builder@users.noreply.github.com>
2021-08-23 17:01:19 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+  
+import kivy
+from kivy.app import App
+from kivy.uix.label import Label
+
+class Myapp(App):
+        def build(self):
+                return Label(text='SreeRam created this app')
+
+Myapp().run()        
